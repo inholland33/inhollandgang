4f3fa7d9ebc8018167f9626fe0bbce31e16a8f59
Sander Boeree <sander.boeree@live.nl>
2019-11-24 16:03:19 +0100

    MVC template project geimport

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+//constants
+define('HASH_GENERAL_KEY', 'MixitUp200');
+
+define('HASH_PASSWORD_KEY', 'catsFLYhigh2000miles');
+
+//database
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'phpP1');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+//paths
+define('URL', 'localhost:63342/test/');
+
+define('LIBS', 'libs/');
---
+++ controllers/manonthemoon.php
@@ -0,0 +1,16 @@
+<?php
+
+class Manonthemoon extends Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function index()
+    {
+        $this->view->render('manonthemoon/index');
+    }
+
+}
