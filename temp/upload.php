4a214e92bddd87a63518ac914784d2db3f2f6210
tachi191 <tachi191@hotmail.com>
2019-11-25 12:01:14 +0100

    max fill size adjustment v2

--- temp/upload.php
+++ temp/upload.php
@@ -20,7 +20,7 @@ if (file_exists($target_file)) {
     $uploadOk = 0;
 }
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 50000000000000) {
+if ($_FILES["fileToUpload"]["size"] > 90000000000000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
