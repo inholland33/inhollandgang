f967d32e0ef674cbc785664172ef438aee2575f1
tachi191 <tachi191@hotmail.com>
2019-12-01 01:57:23 +0100

    show all images button and show what you have uploaded

--- temp/images.php
+++ temp/images.php
@@ -7,6 +7,9 @@
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
 </form>
-
+<form action="all_images.php" method="post" enctype="multipart/form-data">
+    Show all uploaded images:
+    <input type="submit" value="Show all Image" name="submit">
+</form>
 </body>
 </html>
--- temp/upload.php
+++ temp/upload.php
@@ -4,6 +4,7 @@ $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
+
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
@@ -20,7 +21,7 @@ if (file_exists($target_file)) {
     $uploadOk = 0;
 }
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 90000000000000) {
+if ($_FILES["fileToUpload"]["size"] > 50000000000000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
@@ -37,7 +38,9 @@ if ($uploadOk == 0) {
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        echo '<img src="'.$target_file .'" alt="Random image" />'."<br /><br />";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
+
 }
