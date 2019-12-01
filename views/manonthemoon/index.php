fcf8d3645fff69b8a180586bc8b4a87dfb0cb4db
marjolein kassens <marjolein.kassens@hotmail.com>
2019-12-01 13:52:36 +0100

    testen

--- views/manonthemoon/index.php
+++ views/manonthemoon/index.php
@@ -2,4 +2,4 @@
 <p>
     NASA sent the humans to the moon.
 </p>
-<img src='http://2.bp.blogspot.com/-sa-Thd10a_A/TidSYccMpzI/AAAAAAAATBg/AtWkcI5lb-A/s1600/69apollo11.jpg'></img>
+<img src=''></img>http://2.bp.blogspot.com/-sa-Thd10a_A/TidSYccMpzI/AAAAAAAATBg/AtWkcI5lb-A/s1600/69apollo11.jpg
