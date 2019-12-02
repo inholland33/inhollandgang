fec5c1f5b30596c75f5c523d6cd09c99f98cb8e3
rojibos <43674827+rojibos@users.noreply.github.com>
2019-12-02 09:49:46 +0100

    Jazz pagina aangemaakt maar werkt nog niet. -> meer compleet

--- views/jazz/index.php
+++ views/jazz/index.php
@@ -2,11 +2,33 @@
 <div class = "container">
     <h1>
         <img src="#" alt="jazzheader">
-        <?php echo $this->text[0] ?>
-        <p id="ondertitel"><?php echo $this->text[1] ?></p>
+       HEADER
+        <p id="ondertitel">onder de header</p>
     </h1>
     <section class="inleiding">
-        <h2><?php echo $this->text[2] ?></h2>
-        <p><?php echo $this->text[3] ?></p>
+        <h2>INLEIDING KOP</h2>
+        <p>INLEIDING</p>
     </section>
+    <article id="mainhall">
+        <h2>mainhall kop</h2>
+        <hr>
+        <p>mainhall tekst</p>
+    </article>
+    <article id="secondhall">
+        <h2>secondhall kop</h2>
+        <hr>
+        <p>secondhall tekst</p>
+    </article>
+    <article id="thirdhall">
+        <h2>thirdhall kop</h2>
+        <hr>
+        <p>thirdhall tekst</p>
+    </article>
+    <article id="grote markt">
+        <h2>grote markt kop</h2>
+        <hr>
+        <p>grote markt tekst</p>
+    </article>
+    <button type="button" id="gototicketpage">tickets</button>
+    <button type="button" id="gotoprogrammpage">programm</button>
 </div>
