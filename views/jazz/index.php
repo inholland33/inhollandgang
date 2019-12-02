ca60d91ac5b2faed1a244d5e7b89c0ac0bfd2559
rojibos <43674827+rojibos@users.noreply.github.com>
2019-12-02 09:29:06 +0100

    Jazz pagina aangemaakt maar werkt nog niet.

---
+++ views/jazz/index.php
@@ -0,0 +1,12 @@
+
+<div class = "container">
+    <h1>
+        <img src="#" alt="jazzheader">
+        <?php echo $this->text[0] ?>
+        <p id="ondertitel"><?php echo $this->text[1] ?></p>
+    </h1>
+    <section class="inleiding">
+        <h2><?php echo $this->text[2] ?></h2>
+        <p><?php echo $this->text[3] ?></p>
+    </section>
+</div>
