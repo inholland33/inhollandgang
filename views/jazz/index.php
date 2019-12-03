9ec5fc31fe5cfc61f16d84ba2c5a79a9e74dfef2
rojibos <43674827+rojibos@users.noreply.github.com>
2019-12-03 20:16:50 +0100

    Css file aangepast, jazzeventpage is nu compleet qua css

--- views/jazz/index.php
+++ views/jazz/index.php
@@ -1,9 +1,9 @@
 
 <div class = "container">
     <h1>
-        <img src="#" alt="jazzheader">
-       HEADER
-        <p id="ondertitel">onder de header</p>
+        <img src="/inhollandgang/temp/uploads/JazzHeaderImage.jpg" alt="jazzheader" id="headerjazz">
+       <p id="jazztitel">HEADER</p>
+        <p id="jazzondertitel">onder de header</p>
     </h1>
     <section class="inleiding">
         <h2>INLEIDING KOP</h2>
@@ -24,11 +24,13 @@
         <hr>
         <p>thirdhall tekst</p>
     </article>
-    <article id="grote markt">
+    <article id="grotemarkt">
         <h2>grote markt kop</h2>
         <hr>
         <p>grote markt tekst</p>
     </article>
-    <button type="button" id="gototicketpage">tickets</button>
-    <button type="button" id="gotoprogrammpage">programm</button>
+    <section class="navigationbuttons">
+        <button type="button" id="gototicketpage" action="">tickets</button>
+        <button type="button" id="gotoprogrammpage"action="">programm</button>
+    </section>
 </div>
