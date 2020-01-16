7bbdc0e82f6c1df11d36f5decea6ba5e752b8565
FrankW1994 <43065804+FrankW1994@users.noreply.github.com>
2020-01-16 16:44:26 +0100

    inc

---
+++ 3lagen/includes/ticketpageInc.php
@@ -0,0 +1,19 @@
+<?php
+require_once '../models/Ticket_model.php';
+session_start();
+if (isset($_POST['add']))
+{
+    if (!isset($_SESSION['ticketsCart'])) {
+        $_SESSION['ticketsCart'] = array();
+        //$item = array("ticketId" => $_GET['addticket'], "amount" => $_POST['amount'], "price"=>$_POST['price']);
+        $ticket = new TicketModel($ticketId = $_GET['addticket'], $ticketName = $_GET['ticket'], $ticketAmount = $_POST['amount'], $ticketPrice = $_POST['price']);
+        $_SESSION['ticketsCart'][] = $ticket;
+        header("Location: ../view/ticketpage.php");
+    }
+    else{
+        $ticket = new TicketModel($ticketId = $_GET['addticket'], $ticketName = $_GET['ticket'], $ticketAmount = $_POST['amount'], $ticketPrice = $_POST['price']);
+        //$item = array("ticketId" => $_GET['addticket'], "amount" => $_POST['amount'], "price"=>$_POST['price']);
+        $_SESSION['ticketsCart'][] = $ticket;
+        header("Location: ../view/ticketpage.php?ticketid=".$_GET['addticket']."&ticketname=".$_GET['ticket']);
+    }
+}
